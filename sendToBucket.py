ba1d7e6b6dcb2d5c2449cc22f8b692471a9e6fc2
ThomasKrumwiede <113312460+ThomasKrumwiede@users.noreply.github.com>
2022-12-05 12:52:23 -0500

    Add files via upload

---
+++ sendToBucket.py
@@ -0,0 +1,16 @@
+import boto3
+s3_resource = boto3.resource('s3')
+
+''''
+ s3 = boto3.resource('s3',
+         aws_access_key_id=ACCESS_ID,
+         aws_secret_access_key= ACCESS_KEY)
+
+ '''
+
+#output to bucket
+file_name='test.txt'
+bucket_name = 'student5-bucket'
+
+s3.Object('student5-bucket', first_file_name).upload_file(
+    Filename=first_file_name)

14ebd4c5810ddd861655f228ebf37e1c0d9e668c
ThomasKrumwiede <113312460+ThomasKrumwiede@users.noreply.github.com>
2022-12-12 00:39:40 -0500

    Update sendToBucket.py

--- sendToBucket.py
+++ sendToBucket.py
@@ -2,7 +2,7 @@ import boto3
 s3_resource = boto3.resource('s3')
 
 ''''
- s3 = boto3.resource('s3',
+s3 = boto3.resource('s3',
          aws_access_key_id=ACCESS_ID,
          aws_secret_access_key= ACCESS_KEY)
 
